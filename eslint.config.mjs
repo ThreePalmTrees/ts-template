8d5ff1d07cc557d6f52efdfc8b6f90705d4425f2
Admministrator <admministrator@Admministrators-MacBook-Pro.local>
2025-01-30 16:33:27 +0100

    warn for prettier instead of error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,45 +4,45 @@ import typescriptParser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier';
 
 export default [
-    js.configs.recommended,
-    {
-        files: ['**/*.{js,ts}'],
-        languageOptions: {
-            parser: typescriptParser,
-            parserOptions: {
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-            },
-            globals: {
-                // Node.js globals
-                console: 'readonly',
-                exports: 'readonly',
-                module: 'readonly',
-                process: 'readonly',
-                __dirname: 'readonly',
-                require: 'readonly',
-                // Browser globals
-                window: 'readonly',
-                document: 'readonly',
-                // ES2022 globals
-                Promise: 'readonly',
-                Set: 'readonly',
-                Map: 'readonly',
-                WeakMap: 'readonly',
-                WeakSet: 'readonly',
-                BigInt: 'readonly',
-                globalThis: 'readonly'
-            }
-        },
-        plugins: {
-            '@typescript-eslint': typescript,
-            'prettier': prettier,
-        },
-        rules: {
-            ...typescript.configs.recommended.rules,
-            'prettier/prettier': 'error',
-            'no-console': 'off',
-            'no-undef': 'off' // TypeScript handles this better
-        }
-    }
-]; 
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        // Node.js globals
+        console: 'readonly',
+        exports: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        require: 'readonly',
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        // ES2022 globals
+        Promise: 'readonly',
+        Set: 'readonly',
+        Map: 'readonly',
+        WeakMap: 'readonly',
+        WeakSet: 'readonly',
+        BigInt: 'readonly',
+        globalThis: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      prettier: prettier,
+    },
+    rules: {
+      ...typescript.configs.recommended.rules,
+      'prettier/prettier': 'warn',
+      'no-console': 'off',
+      'no-undef': 'off', // TypeScript handles this better
+    },
+  },
+];
